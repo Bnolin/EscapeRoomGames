0893c10d349ca830d64b536ecfaa6c12ca4e6767
Bnolin <bnolin2411@gmail.com>
2018-08-01 12:22:08 -0400

    Initial Commit of Escape Room Games Website

---
+++ Escaperoomscripts.js
@@ -0,0 +1,15 @@
+﻿function ButtonOver(buttonId) {
+    var button = $('#Button' + buttonId);
+
+    if (button !== null) {
+        button.removeClass("Button").addClass("ButtonHover");
+    }
+}
+
+function ButtonOut(buttonId) {
+    var button = $('#Button' + buttonId);
+
+    if (button !== null) {
+        button.removeClass("ButtonHover").addClass("Button");
+    }
+}
