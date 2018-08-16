6cbf3da3d498cd393e16845d5c41713182bca987
Brian Nolin <bnolin2411@gmail.com>
2018-08-15 23:59:18 -0400

    Homework #4 - Initial Commit of JS code

--- Escaperoomscripts.js
+++ Escaperoomscripts.js
@@ -1,15 +1,46 @@
-﻿function ButtonOver(buttonId) {
-    var button = $('#Button' + buttonId);
+function updateDatabase() {
+ 
+    /* -------------------------------------------*/
+    /* PSEUDO CODE */
+    /* -------------------------------------------*/
+    /*
+        1) Create and send request to get all emails from database
+        3) Compare emails with input
+        4) If unique:
+            - Create and send request to add email to database
+            - Alert with success!
+           If already there:
+            - Alert with try again!
+    */
+    /* -------------------------------------------*/
+    
+    // Create XMLHttpRequest
+    // var getRequest = new XMLHttpRequest();
+    // getRequest.open("GET", "database.xml", true);
+    // getRequest.send();
+    // var xml = getRequest.responseXML;
 
-    if (button !== null) {
-        button.removeClass("Button").addClass("ButtonHover");
-    }
+    // var emails = xml.getElementsByTagName("email"); 
+    // var input = document.getElementById("inputEmail").value;
+    // var unique = true;
+    
+    // Determine if email submitted is unique
+    // for (var i in emails) {
+        // if (i == input) {
+            // unique = false;
+        // }
+    // }
+    
+    // if (unique){
+        // Create email string
+        // var data = '<email>' + input + '</email>';
+        // var postRequest = new XMLHttpRequest();
+        // postRequest.post("POST", "database.xml", true);
+        // postRequest.send(data);
+        // alert("Thank you for subscribing to the newsletter!");
+    // }
+    // else {
+        // alert("Email address already in use, please use another email!");
+    // }
 }
-
-function ButtonOut(buttonId) {
-    var button = $('#Button' + buttonId);
-
-    if (button !== null) {
-        button.removeClass("ButtonHover").addClass("Button");
-    }
-}
+  
